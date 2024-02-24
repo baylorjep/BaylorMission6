8dc7dde64e60f2fed1f97d7fe876a191e5683fb7
Baylor Jeppsen <baylorj@byu.edu>
2024-02-24 11:17:34 -0700

    Forma

---
+++ BaylorMission6/Models/Rating.cs
@@ -0,0 +1,24 @@
+﻿namespace BaylorMission6.Models
+{
+    public class Rating
+    {
+        public int ratingID { get; set; }
+
+        public string category { get; set; }
+
+        public string title { get; set; }
+
+        public int year { get; set; }
+
+        public string director { get; set; }
+
+        public string rating { get; set; }
+
+        public bool edited { get; set; }
+
+        public string lentTo { get; set; }
+
+        public string notes { get; set; }
+
+    }
+}
