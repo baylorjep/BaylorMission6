feb06d35c769b6c6f0447b4249b9e421906f85ac
Baylor Jeppsen <baylorj@byu.edu>
2024-02-24 17:11:57 -0700

    ratingdbupdate

--- BaylorMission6/Migrations/20240224233035_initial.cs
+++ BaylorMission6/Migrations/20240224233035_initial.cs
@@ -21,9 +21,9 @@ namespace BaylorMission6.Migrations
                     year = table.Column<int>(type: "INTEGER", nullable: false),
                     director = table.Column<string>(type: "TEXT", nullable: false),
                     rating = table.Column<string>(type: "TEXT", nullable: false),
-                    edited = table.Column<bool>(type: "INTEGER", nullable: false),
-                    lentTo = table.Column<string>(type: "TEXT", nullable: false),
-                    notes = table.Column<string>(type: "TEXT", nullable: false)
+                    edited = table.Column<bool>(type: "INTEGER", nullable: true),
+                    lentTo = table.Column<string>(type: "TEXT", nullable: true),
+                    notes = table.Column<string>(type: "TEXT", maxLength: 25, nullable: true)
                 },
                 constraints: table =>
                 {
--- BaylorMission6/Models/Rating.cs
+++ BaylorMission6/Models/Rating.cs
@@ -5,28 +5,28 @@ namespace BaylorMission6.Models
     public class Rating
     {
         [Key]
-        [Required]
+        
         public int ratingID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Category is required")]
         public string category { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Title is required")]
 
         public string title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Year is required")]
 
         public int year { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Director is required")]
+        [Range(1900, 2024, ErrorMessage = "Year must be between 1900 and 2024")]
         public string director { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Rating is required")]
+        [RegularExpression(@"^(G|PG|PG-13|R)$", ErrorMessage = "Rating must be G, PG, PG-13, or R")]
         public string rating { get; set; }
 
-        public bool edited { get; set; }
+        public bool? edited { get; set; }
 
-        public string lentTo { get; set; }
-
-        public string notes { get; set; }
+        public string? lentTo { get; set; }
+        [MaxLength(25, ErrorMessage = "Notes cannot be more than 25 characters")]
+        public string? notes { get; set; }
 
     }
 }
